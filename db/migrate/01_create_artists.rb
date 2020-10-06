3b1d6660c9468d04b1b0fbd3016472b5744c4eb7
rncongi <rncongi@gmail.com>
2020-10-06 15:00:01 -0400

    Done.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,19 @@
+class CreateArtists < ActiveRecord::Migration[5.2]
+    def up
+        #do
+    end
+   
+    def down
+        #undo
+    end
+
+    def change
+        #primary way of writing migrations, works for majority of cases, where ActiveRecord knows how to reverse the migration automatically
+        create_table :artists do |t|
+            t.string :name
+            t.string :genre
+            t.integer :age
+            t.string :hometown
+        end 
+    end 
+  end
